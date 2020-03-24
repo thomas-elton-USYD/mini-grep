f7bcf8933d28c52c961e0441a335dbcd2604bdee
Thomas Elton <telt8898@uni.sydney.edu.au>
2020-03-24 18:32:14 +1100

    Updated file.c

--- file.c
+++ file.c
@@ -1,15 +1,26 @@
+//Include libraries
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]){i
+
+	//Isolate search patter arugment
 	char* search_pattern = argv[1];
+
+	//Length of the search patter
 	int pattern_length = (int) strlen(search_pattern);
+
+	//Inialise Variables
 	int display = 0;
 	int pattern_index;
 
 	//Iterate over every word
 	for (int i = 2; i < argc; i++){
+
+		//Isolate each word from the arguments
 		char* word = argv[i];
+
+		//Length of the word previously isolated.
 		size_t word_length = strlen(word);
 
 		//Iterate over every character
@@ -21,8 +32,10 @@ int main(int argc, char* argv[]){
 				//Re-set to default value.
 				pattern_index = 0;
 					
-				//Loop over the pattern and check that characters in word match
+				//Loop over the pattern and check that characters in word match.
 				for (int x = z; x < z+pattern_length; x++){
+
+					//Check that the next characters also fit the search pattern.
 					if (word[x] == search_pattern[pattern_index]){
 						pattern_index++;
 						printf("%d\n", pattern_index);
